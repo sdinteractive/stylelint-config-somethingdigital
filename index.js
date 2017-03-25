c5a58d3a49d990032e5b1003e635f710fe26508e
Nadav Spiegelman <me@nadav.name>
2017-03-25 14:55:07 -0400

    Adjust spacing for at-rules
    Stop yelling about an empty line before includes. E.g., this is good:
    
    ```scss
    .stuff {
      color: red;
    
      @include bp($bp-medium) {
        color: blue;
      }
    }
    ```

--- index.js
+++ index.js
@@ -3,6 +3,15 @@
 module.exports = {
   "extends": "stylelint-config-standard",
   "rules": {
+    "at-rule-empty-line-before": [ "always", {
+      except: [
+        "blockless-after-same-name-blockless",
+        "first-nested",
+        "inside-block",
+      ],
+      ignore: ["after-comment"],
+      ignoreAtRules: ["include"],
+    } ],
     "number-leading-zero": "never",
   }
 }
