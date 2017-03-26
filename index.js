bc3b0defae05f6326064aa90dc5b7f1ba4689ac4
Nadav Spiegelman <me@nadav.name>
2017-03-25 20:45:44 -0400

    Turn off at-rule-empty-line-before
    Can't get it to do what I want.

--- index.js
+++ index.js
@@ -3,15 +3,7 @@
 module.exports = {
   "extends": "stylelint-config-standard",
   "rules": {
-    "at-rule-empty-line-before": [ "always", {
-      except: [
-        "blockless-after-same-name-blockless",
-        "first-nested",
-        "inside-block",
-      ],
-      ignore: ["after-comment"],
-      ignoreAtRules: ["include"],
-    } ],
+    "at-rule-empty-line-before": false,
     "number-leading-zero": "never",
   }
 }
